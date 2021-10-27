1ed4155d5c122b9c372c476e6c23408da4dd0134
George <george.n.brand@icloud.com>
2021-10-26 22:32:48 -0400

    Added Login and Registration pages.  Not tested yet.

--- users/urls.py
+++ users/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     path('login/', 
         auth_views.LoginView.as_view(template_name='users/login.html'),
         name='login'),
-        
+
     # Logout page.
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
     # Registration page. 
-    # path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
 ]
